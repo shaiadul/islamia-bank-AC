2c07402d842a64fa22530e7955ae822a7ad6030f
shaiadul <devshaiadul69@gmail.com>
2022-02-12 18:34:43 +0600

    design

--- javascript/banking.js
+++ javascript/banking.js
@@ -8,7 +8,6 @@ function getInputValue(inputId) {
     inputField.value = '';
     return amountValue;
 }
-
 function updateTotalField(totalFieldId, amount) {
     // debugger;
     const totalElement = document.getElementById(totalFieldId);
@@ -16,17 +15,14 @@ function updateTotalField(totalFieldId, amount) {
     const previousTotal = parseFloat(totalText);
     totalElement.innerText = previousTotal + amount;
 }
-
 function getCurrentBalance() {
     const balanceTotal = document.getElementById('balance-total');
     const balanceTotalText = balanceTotal.innerText;
     const previousBalanceTotal = parseFloat(balanceTotalText);
     return previousBalanceTotal;
 }
-
 function updateBalance(amount, isAdd) {
     const balanceTotal = document.getElementById('balance-total');
-  
     const previousBalanceTotal = getCurrentBalance();
     if (isAdd == true) {
         balanceTotal.innerText = previousBalanceTotal + amount;
@@ -35,19 +31,16 @@ function updateBalance(amount, isAdd) {
         balanceTotal.innerText = previousBalanceTotal - amount;
     }
 }
-
+//handel deposite button
 document.getElementById('deposit-button').addEventListener('click', function () {
-
     const depositAmount = getInputValue('deposit-amount');
     if (depositAmount > 0) {
         updateTotalField('deposit-total', depositAmount);
         updateBalance(depositAmount, true);
     }
 });
-
 // handle withdraw button 
 document.getElementById('withdraw-button').addEventListener('click', function () {
-   
 
     const withdrawAmount = getInputValue('withdraw-amount');
     const currentBalance = getCurrentBalance();
